66149c4b4315d25d5d1e6085fde5a77bb9997eb9
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-21 01:01:27 +0800

    Create admin.php

---
+++ myweb/www/admin.php
@@ -0,0 +1,72 @@
+<?php
+require_once('checklogin.php');
+if($login){
+    $token = CSRFtokenGenerator();
+
+    include('config.php');
+    $stmt = $link->prepare("SELECT admin FROM users WHERE username = ? and password = ?");
+    $stmt->bind_param("ss", $_COOKIE['username'], $_COOKIE['password']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    mysqli_close($link);
+    $row = mysqli_fetch_array($result);
+    
+
+    if($row['admin']==1){
+        if( !(!isset($_POST['title']) || $_POST['title']=="") ){
+            $post_token = $_POST['token'];
+            if($post_token != $_COOKIE['CSRF_token']){
+                echo " <script   language = 'javascript' 
+                type = 'text/javascript'> "; 
+                echo " top.location.href = 'chat.php' "; 
+                echo " </script > "; 
+                exit;
+            }
+            else{
+                $title = $_POST['title'];
+
+                include('config.php');
+                $stmt = $link->prepare("UPDATE title SET head=?");
+                $stmt->bind_param("s", $title);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                mysqli_close($link);
+                $row = mysqli_fetch_array($result);
+                echo '更改成功<br>';
+            }
+        }
+    }
+    else{
+        header("Location: chat.php");
+        exit;
+    }
+
+    echo'  
+    <button onclick="prepage()"> 上一頁 </button><br>
+    <h4>更換主頁標題</h4>
+    <form method="POST" action="admin.php">
+       <input id="title" placeholder="title" required="" autofocus="" type="text" name="title">
+       <input type="hidden" name="token" value="'.$token.'"/>
+       <button  type="submit">更改</button>
+    </form>
+    ';
+}
+
+function CSRFtokenGenerator($len = 16){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $token = '';
+    for($i=0;$i<$len;$i++){
+        $token .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    header("Set-Cookie: CSRF_token=".urlencode($token)."; HttpOnly; Secure; SameSite=strict", false);
+    return $token;
+}
+
+?>
+
+
+<script>
+function prepage(){
+    window.location.href="chat.php"
+}
+</script>
