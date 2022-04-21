677504f3040f267ea774899976896633d37a79b2
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-22 01:17:39 +0800

    Update login.php

--- myweb/www/login.php
+++ myweb/www/login.php
@@ -5,7 +5,23 @@ if( !isset($_POST['username']) || !isset($_POST['password']) || $_POST['username
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-require_once('config.php');
+include('config.php');
+$stmt = $link->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+mysqli_close($link);
+$row = mysqli_fetch_array($result);
+if($row['unlocktime']){
+    $date=new DateTime();
+    $second=strtotime($row['unlocktime']) - strtotime($date->format('Y-m-d H:i:s'));
+    if( $second > 0 ){
+        echo '請'.$second.'秒後再試<br>';
+        exit;
+    }
+}
+
+include('config.php');
 $stmt = $link->prepare("SELECT * FROM users WHERE username = ? and password = ?");
 $stmt->bind_param("ss", $username, $password);
 $stmt->execute();
@@ -26,6 +42,12 @@ try {
     }else{
         echo '登入失敗';
         echo '<button onclick="goback()"> 上一頁 </button>';
+
+        include('config.php');
+        $stmt = $link->prepare("UPDATE users SET unlocktime = date_add(now(), interval 10 second) WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        mysqli_close($link);
     }
 }
 catch (Exception $e) {
