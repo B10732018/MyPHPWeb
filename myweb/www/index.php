e24a066606e03bacd712e6c4fd98d6ba7790beea
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-25 01:44:20 +0800

    Update index.php

--- myweb/www/index.php
+++ myweb/www/index.php
@@ -26,7 +26,7 @@ if($_COOKIE['username'] && $_COOKIE['password']){
             }
         }
         catch (Exception $e) {
-            echo 'Caught exception: ', str_replace("&","&amp;",str_replace(">","&gt",str_replace("<","&lt",$e->getMessage()))), '<br>';
+            echo 'Caught exception: ', str_replace("<","&lt",str_replace(">","&gt",str_replace("&","&amp;",$e->getMessage()))), '<br>';
             echo 'Check credentials in config file at: ', $Mysql_config_location, '\n';
         }
     }
