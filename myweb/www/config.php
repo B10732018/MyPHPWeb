1de7177afe81677ed434643370ae9042b8747bd9
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-25 01:41:52 +0800

    Update config.php

--- myweb/www/config.php
+++ myweb/www/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'db');
-define('DB_USERNAME', 'user');
-define('DB_PASSWORD', 'PSD_ntust_B10732018');
+define('DB_USERNAME', 'userB10732018');
+define('DB_PASSWORD', 'PnStDB10u732s018t');
 define('DB_NAME', 'myDb');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
