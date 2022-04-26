224e18cc2d6678783a0a073fc353c777baa7886d
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-26 15:47:16 +0800

    Update logout.php

--- myweb/www/logout.php
+++ myweb/www/logout.php
@@ -3,5 +3,7 @@ header("Location: index.php");
 setcookie("username", "", time()-3600);
 setcookie("password", "", time()-3600);
 setcookie("id", "", time()-3600);
+setcookie("CSRF_token", "", time()-3600);
+setcookie("CSRF_refresh_time", "", time()-3600);
 
 ?>
