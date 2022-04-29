507866989dc36d8ee18ad7f430dc0101e006ea29
B10732018 <71759401+B10732018@users.noreply.github.com>
2022-04-29 14:48:23 +0800

    Update logout.php

--- myweb/www/logout.php
+++ myweb/www/logout.php
@@ -1,4 +1,17 @@
 <?php
+$token = $_GET['token'];
+
+if($token != $_COOKIE['CSRF_token']){
+    echo "CSRF_token don't match";
+    echo '<script>
+            function prepage(){
+                window.location.href="chat.php"
+            }
+            </script>
+            <button onclick="prepage()"> 上一頁 </button><br>';
+    exit;
+}
+
 header("Location: index.php");
 setcookie("username", "", time()-3600);
 setcookie("password", "", time()-3600);
